3ec9eb7deea7bbbb45b6679174ffb24634e5e5e4
Omran Jamal <o.jamal97@gmail.com>
2025-08-14 02:45:05 +0600

    added name & color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { configure } from '@airstate/client';
 import { usePersistentNanoId, useSharedPresence, useSharedState } from '@airstate/react';
 import { Switch } from './components/Switch.tsx';
-import { Darkness } from './components/Darkness.tsx';
+import { Overlay } from './components/Overlay.tsx';
 import clsx from 'clsx';
 
 if (!import.meta.env.VITE_AIRSTATE_APP_ID) {
@@ -17,28 +17,35 @@ const url = new URL(window.location.href);
 function App() {
     const peerId = usePersistentNanoId(url.searchParams.get('peer-id-key') ?? 'default-peer');
 
-    const [state, setState] = useSharedState<'on' | 'off'>('off', {
+    const [state, setState] = useSharedState<'on' | 'off'>('on', {
         channel: `demo_light-switch_${url.pathname}_state`,
     });
 
     const {
-        setState: setCursorState,
+        setState: setPresenceState,
         self,
         others,
-    } = useSharedPresence<[number, number]>({
+    } = useSharedPresence<{ name: string; color: string; pos: [number, number] }>({
         peerId: peerId,
         room: `demo_light-switch_${url.pathname}_presence`,
-        initialState: [0, 0],
+        initialState: {
+            name: url.searchParams.get('name') ?? 'user',
+            color: url.searchParams.get('suggested-color') ?? 'hsl(255, 50%, 50%)',
+            pos: [2048, 2048],
+        },
     });
 
     const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
         const center = [window.innerWidth / 2, window.innerHeight / 2];
-        setCursorState([e.clientX - center[0], e.clientY - center[1]]);
+        setPresenceState((state) => ({ ...state, pos: [e.clientX - center[0], e.clientY - center[1]] }));
     };
 
     const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
         const center = [window.innerWidth / 2, window.innerHeight / 2];
-        setCursorState([e.touches[0].clientX - center[0], e.touches[0].clientY - center[1]]);
+        setPresenceState((state) => ({
+            ...state,
+            pos: [e.touches[0].clientX - center[0], e.touches[0].clientY - center[1]],
+        }));
     };
 
     const otherLights = Object.values(others)
@@ -51,11 +58,33 @@ function App() {
         <div
             onMouseMove={handleMouseMove}
             onTouchMove={handleTouchMove}
+            onMouseLeave={() => {
+                setPresenceState((state) => ({
+                    ...state,
+                    pos: [2048, 2048],
+                }));
+            }}
             className={clsx(
-                'fixed touch-none top-0 left-0 w-screen h-screen bg-white flex items-center justify-center',
+                'cursor-none select-none fixed touch-none top-0 left-0 w-screen h-screen bg-white flex items-center justify-center',
             )}>
-            <Darkness centers={[ownLight, ...otherLights]} isDark={state === 'off'} />
+            <Overlay centers={[ownLight, ...otherLights]} isDark={state === 'off'} />
             <Switch state={state} onChange={setState} />
+            <div className={'absolute bottom-5 right-40'}>
+                <input
+                    className={clsx(
+                        'w-24 border-2 px-2 py-1 text-sm outline-0 rounded-full bg-white',
+                        // state === 'on' ? 'bg-white' : 'bg-black',
+                    )}
+                    value={self.state.name}
+                    style={{ borderColor: self.state.color, color: self.state.color }}
+                    onChange={(ev) => {
+                        setPresenceState((state) => ({
+                            ...state,
+                            name: ev.target.value,
+                        }));
+                    }}
+                />
+            </div>
         </div>
     );
 }
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -3,17 +3,21 @@ import clsx from 'clsx';
 
 export const TORCH_DIAMETER = 250;
 
-export function Darkness({ centers, isDark }: { centers: [number, number][]; isDark: boolean }) {
+export function Overlay({
+    centers,
+    isDark,
+}: {
+    centers: {
+        name: string;
+        color: string;
+        pos: [number, number];
+    }[];
+    isDark: boolean;
+}) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
 
     const [r, forceRender] = useReducer((x) => x + 1, 0);
-
-    const centersRef = useRef(centers);
-
-    useEffect(() => {
-        centersRef.current = centers;
-    }, [centers]);
 
     const render = useCallback(
         (centers: [number, number][]) => {
@@ -67,7 +71,7 @@ export function Darkness({ centers, isDark }: { centers: [number, number][]; isD
     );
 
     useEffect(() => {
-        render(centers);
+        render(centers.map((center) => center.pos));
     }, [centers, r, render]);
 
     useEffect(() => {
@@ -77,14 +81,37 @@ export function Darkness({ centers, isDark }: { centers: [number, number][]; isD
     }, []);
 
     return (
-        <canvas
-            className={clsx(
-                'absolute w-full h-full top-0 left-0 z-10 pointer-events-none transition-opacity',
-                isDark ? 'opacity-100' : 'opacity-0',
-            )}
-            ref={canvasRef}
-            width={window.innerWidth}
-            height={window.innerHeight}
-        />
+        <div className={'absolute w-full h-full top-0 left-0 z-10 pointer-events-none'}>
+            <canvas
+                className={clsx(
+                    'absolute w-full h-full top-0 left-0 transition-opacity',
+                    isDark ? 'opacity-100' : 'opacity-0',
+                )}
+                ref={canvasRef}
+                width={window.innerWidth}
+                height={window.innerHeight}
+            />
+            {centers.map((center) => {
+                const x = Math.round(window.innerWidth / 2 + center.pos[0]);
+                const y = Math.round(window.innerHeight / 2 + center.pos[1]);
+
+                return (
+                    <>
+                        <div
+                            style={{ left: x, top: y, background: center.color }}
+                            className={'absolute w-2 h-2 -translate-1 rounded-full'}></div>
+                        <div
+                            style={{
+                                left: x,
+                                top: y,
+                                background: center.color,
+                            }}
+                            className={clsx('absolute translate-2 shadow text-white text-sm px-2 py-1 text-nowrap')}>
+                            {center.name}
+                        </div>
+                    </>
+                );
+            })}
+        </div>
     );
 }
